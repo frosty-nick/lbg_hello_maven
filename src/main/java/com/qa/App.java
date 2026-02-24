6721f7079928c4ebc765de1b086afebb1df52dcc
Nick <chief.spare1f@icloud.com>
2026-02-24 14:28:53 +0000

    EDIT: Add further  hello message

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -14,6 +14,7 @@ public class App
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayHelloToSomeone("You"));
+        System.out.println(sayHelloToSomeone("Matt"));
         System.out.println(sayGoodbye());
     }
 
